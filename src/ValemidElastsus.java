86e81ed94c7a5bdaec2faf30e7d56bb967a7f3e5
hanna <hannah.kaarma@gmail.com>
2024-03-21 00:35:24 +0200

    test

--- src/ValemidElastsus.java
+++ src/ValemidElastsus.java
@@ -1,3 +1,60 @@
 public class ValemidElastsus {
+    private long H1;
+    private long H2;
+    private long Q1;
+    private long Q2;
 
+    public ValemidElastsus(long h1, long h2, long q1, long q2) {
+        H1 = h1;
+        H2 = h2;
+        Q1 = q1;
+        Q2 = q2;
+    }
+    public long hinnaelastsus(){
+        long elastsus=((Q2-Q1)/Q1)/((H2-H1)/H1);
+        if (elastsus<0)elastsus=elastsus*-1;
+        if (H2>H1) {
+            if (elastsus == 1) {
+                System.out.println("Sinu toote/teenuse hinnaelastsus on " + elastsus + ", ehk on ühikelastne.");
+                System.out.println("See tähendab, et kui tõstsid hinda, siis kogus muutub täpselt samas proportsioonis, aga vastassuunas, sama kehtib hinda alandades");
+                System.out.println("Soovitame hinnakujunduse pidevat jälgimist ja kohandamist vastavalt turuolukorrale!");
+            }
+            if (elastsus < 1) {
+                System.out.println("Sinu toote/teenuse hinnaelastsus on " + elastsus + ", ehk on mitte elastne.");
+                System.out.println("See tähendab, et sinu toode/teenus pole nii tundlik hinnamuutusele");
+                System.out.println("Soovitame sul jätkata oma praguse hinnatõusu strateegiaga, aga mõistlikuse piires!");
+                System.out.println("Võiks ka kaaluda kvaliteedi või lisaväärtuse suurendamist veelgi, et õigustada kõrgemat hinda ja suurendada kasumit");
+            }
+            if (elastsus > 1) {
+                System.out.println("Sinu toote/teenuse hinnaelastsus on " + elastsus + ", ehk on elastne.");
+                System.out.println("See tähendab, et sinu toode/teenus on üsna tundlik hinnamuutusele");
+                System.out.println("Soovitame sul hinda pigem mitte tõsta, sest nii väheneb su müügikogus");
+                System.out.println("Võiks ka kaaluda oma toote/teenuse eristamist teistest konkurentidest, et tarbija kohe hinna väiksemagi tõusu juures teise odavama tootja juurde üle ei läheks");
+
+
+            }
+        }
+        if (H2<H1) {
+            if (elastsus == 1) {
+                System.out.println("Sinu toote/teenuse hinnaelastsus on " + elastsus + ", ehk on ühikelastne.");
+                System.out.println("See tähendab, et kui muutsid hinda odavamaks, siis kogus muutub täpselt samas proportsioonis, aga vastassuunas, sama kehtib hinda tõstes");
+                System.out.println("Soovitame hinnakujunduse pidevat jälgimist ja kohandamist vastavalt turuolukorrale!");
+            }
+            if (elastsus < 1) {
+                System.out.println("Sinu toote/teenuse hinnaelastsus on " + elastsus + ", ehk on mitte elastne.");
+                System.out.println("See tähendab, et sinu toode/teenus pole nii tundlik hinnamuutusele");
+                System.out.println("Soovitame sul hinda pigem tõsta, aga mõistlikuse piires!");
+                System.out.println("Võiks ka kaaluda kvaliteedi või lisaväärtuse suurendamist, et õigustada kõrgemat hinda ja suurendada kasumit");
+            }
+            if (elastsus > 1) {
+                System.out.println("Sinu toote/teenuse hinnaelastsus on " + elastsus + ", ehk on elastne.");
+                System.out.println("See tähendab, et sinu toode/teenus on üsna tundlik hinnamuutusele");
+                System.out.println("Soovitame sul jätkata praguse strateegiaga, et pigem hinda alanadada, sest nii suureneb su müügikogus");
+                System.out.println("Võiks ka kaaluda oma toote/teenuse eristamist teistest konkurentidest, et ei peaks kõige odavamat toodet turul hoidma. See võib küll koguseid suurendada, aga brändi kuvand jääb pigem odav ja mittekvaliteetne");
+
+
+            }
+        }
+        return elastsus;
+    }
 }
--- src/maindev.java
+++ src/maindev.java
@@ -41,35 +41,47 @@ public class maindev {
     }
 
     private static void ressursideOptimiseerimine() {
-        System.out.println("ressursid");
+        System.out.println("Kõigepealt vajame mõningaid andmeid, et teha vajalikud arvutused");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Palju sul oli aasta tagasi töötajaid");
         int L1 = scanner.nextInt();
-        System.out.println(L1);
         System.out.println("Palju oli sul aasta tagasi kapitali (masinad, töövahendid jne)?");
         int K1 = scanner.nextInt();
-        System.out.println(K1);
         System.out.println("Palju on sul praegu töötajaid?");
         int L2 = scanner.nextInt();
-        System.out.println(L2);
         System.out.println("Palju on sul praegu kapitali?");
         int K2 = scanner.nextInt();
-        System.out.println(K2);
         System.out.println("Palju muutus sinu tootmiskogus võrreldes eelmise aastaga?");
         int deltaQ = scanner.nextInt();
-        System.out.println(deltaQ);
         System.out.println("Palju maksab praegu üks ühik kapitali?");
         int r = scanner.nextInt();
-        System.out.println(r);
         System.out.println();
         ValemidPalkKogukulu ressurss = new ValemidPalkKogukulu(deltaQ, K1, K2, L1, L2, r);
         System.out.println(ressurss);
-        System.out.println(ressurss.kogukulu());
+        System.out.println("Palgakulu peaks olema "+ressurss.palk()+" optimaalse tootmise juures");
+        System.out.println("Sinu ettevõtte kogukulu optimaalse tootmise juures peaks olema: "+ressurss.kogukulu());
 
     }
 
     private static void hinnaMuutmine() {
-        System.out.println("hinnamuutmine");
+        System.out.println("Kõigepealt vajame mõningaid andmeid, et teha vajalikud arvutused");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sisesta, mis oli su toote/teenuse hind enne hinna muutmist?");
+        long H1 = scanner.nextInt();
+        System.out.println("Sisesta, mis oli su toote/teenuse hind peale hinna muutmist?");
+        long H2 = scanner.nextInt();
+        System.out.println("Palju müüsid esialgse hinna juures?");
+        long Q1 = scanner.nextInt();
+        System.out.println("Palju müüsid muudetud hinna juures?");
+        long Q2 = scanner.nextInt();
+
+        System.out.println();
+        ValemidElastsus andmed = new ValemidElastsus(H1, H2, Q1, Q2);
+        //System.out.println(andmed);
+
+        andmed.hinnaelastsus();
+
+
 
     }
 }
